7025e98ba72d4f6d9e68b47afad18cfcfb19f72c
eunju-baek <77427870+eunju-baek@users.noreply.github.com>
2022-04-27 10:58:42 +0900

    Data cleansing

---
+++ ENV.R
@@ -0,0 +1,30 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+phenotype = args[1]
+environment = args[2]
+
+
+library(data.table)
+library(dplyr)
+
+
+pheno <- fread(phenotype)
+
+env <- fread(environment)
+
+names(env)[1] <- "FID"
+
+
+a <- left_join(pheno,env,by= "FID")
+
+
+#you can customize filter
+
+filt <- paste(names(env)[1:ncol(env)],collapse=">=0&")
+
+filt <- as.logical(filt)
+
+a <-filter(a,filt)
+
+fwrite(a,'pheno_QCed.txt',quote=F,sep='\t',row.names=F,na="NA")
